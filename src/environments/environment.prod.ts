ffbabe0989dfcfea008077fd9f66916227527211
Jerish Bovas <jerishbradlyb@gmail.com>
2023-06-03 14:02:33 -0400

    firebase key added for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyByl9ksNl5xMbU82ayKYuYYbGYWS2iJz5c",
+    authDomain: "recipegpt-app.firebaseapp.com",
+    projectId: "recipegpt-app",
+    storageBucket: "recipegpt-app.appspot.com",
+    messagingSenderId: "777577960268",
+    appId: "1:777577960268:web:487ace774a741af481f5e9",
+    measurementId: "G-NJZR5XFBX8"
+  }
 };
